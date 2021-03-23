8adf0d04573297d4f2ad6cffe7b1f2dcdb438eb0
vasuskimo <vasuskimo@users.noreply.github.com>
2021-03-22 22:27:55 -0700

    Create SmartPtr.cpp

---
+++ SmartPtr.cpp
@@ -0,0 +1,37 @@
+using namespace std;
+#include <iostream>
+
+#include <iostream> 
+using namespace std; 
+
+class SmartPtr 
+{ 
+	int* ptr;
+	
+	public: 
+
+	explicit SmartPtr(int* p = NULL) 
+	{ 
+		ptr = p; 
+	} 
+
+	~SmartPtr() 
+	{ 
+		cout << "Destructor called" << endl;
+		delete (ptr); 
+	} 
+
+	// Overloading dereferencing operator 
+	int& operator*() 
+	{ 
+		return *ptr; 
+	} 
+}; 
+
+int main() 
+{ 
+	SmartPtr p(new int()); 
+	*p = 20; 
+	cout << *p << endl; 
+	return 0; 
+}
